3af68263910b1decd7ee8fb8faefb5fa8ae0621f
Aaron Plavnick <plavadp@gmail.com>
2015-01-18 13:08:08 -0600

    get parallel map working

--- date.rb
+++ date.rb
@@ -2,6 +2,8 @@ require 'future'
 require 'nokogiri'
 require 'open-uri'
 require 'chronic'
+require 'parallel'
+require 'ruby-progressbar'
 require 'date'
 require 'set'
 
@@ -15,11 +17,11 @@ urls = { 'GE' => 'http://www.ge.com/investor-relations/events',
          'ADBE' => 'http://www.adobe.com/investor-relations/calendar.html',
          'AAPL' => 'http://investor.apple.com/',
          'BHI' => 'http://phx.corporate-ir.net/phoenix.zhtml?c=79687&p=irol-irhome',
-         
+         'CMG' => 'http://ir.chipotle.com/phoenix.zhtml?c=194775&p=irol-news',
         }
     
 def get_dates(url)
-    html = Nokogiri::HTML(open(url))
+    html = Nokogiri::HTML(open(url, :read_timeout=>10))
     month = /([Jj]an[\s])|([Ff]eb[\s])|([Mm]ar[\s])|([Aa]pr[\s])|([Mm]ay[\s])|([Jj]un[\s])|([Jj]ul[\s])|([Aa]ug[\s])|([Ss]ep[\s])|([Oo]ct[\s])|([Nn]ov[\s])|([Dd]ec[\s])|([Jj]anuary[\s])|([Ff]ebruary[\s])|([Mm]arch[\s])|([Aa]pril[\s])|([Mm]ay[\s])|([Jj]une[\s])|([Jj]uly[\s])|([Aa]ugust[\s])|([Ss]eptember[\s])|([Oo]ctober[\s])|([Nn]ovember[\s])|([Dd]ecember[\s])/
     numerical_date = /(\d{1,2}[\/\- ]+\d{1,2}[\/\- ]+\d{2,4})|(\d{2,4}[\/\- ]+\d{1,2}[\/\- ]+\d{1,2})|([ ]+\d{1,2}.*201[5-9])|([ ]+\d{1,2}.*20[2-9][0-9])/
     number = /\d/ 
@@ -48,37 +50,24 @@ def get_dates(url)
     return dates
 end
 
+results = Parallel.map(urls, :in_threads=>2){ |stock, url| { stock => get_dates(url) } }
 
-urls.each do |stock, url| 
-    dates = get_dates(url)
-    dates.each do |date, strings|
-        puts stock, date
-        strings.each do |string|
-            puts string
+results_hash = Hash.new
+
+results.each do |result|
+    results_hash = results_hash.merge(result)
+end
+
+results_hash.each do |stock, dates|
+    puts stock
+    if dates != nil
+        dates.each do |date, strings|
+            puts date
+            strings.each do |string| 
+                puts string 
+            end
         end
     end
 end
 
-
-#puts text
-
-
-#re = /<("[^"]*"|'[^']*'|[^'">])*>/
-#text = html.to_html.gsub(re,",").gsub(/\n/,'').squeeze(",")
-
-=begin
-text = text.gsub(/[\s]/,',').gsub(/,+/,',').gsub(/,\D+,/,',')
-
-#puts text
-
-text = text.split(/,/)
-
-
-text.each do |line|
-    date = Chronic.parse(line.strip)
-    if date != nil # and date.to_date > Date.today
-        dates.add(date)
-    end
-end
-=end
 
